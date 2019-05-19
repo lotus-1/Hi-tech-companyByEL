9072d31eb53be6b877b9c96c4d0f46327e297051
enasskmal <enasskmal@gmail.com>
2019-05-19 09:54:49 +0300

    fixing issues #16 #17 #18 #21

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -4,9 +4,6 @@ const dbConnection = require("./db_connection.js");
 
 let sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
-function lowercase(value) {
-  return value.toLowerCase()
-};
 
 
 module.exports = {
